fb0dfbccb6dac58e9c76384efcbef9b50d17d44f
Satyam Trivedi <satyamtrivedi111@gmail.com>
2024-05-01 01:28:32 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main{
+
+  public static void main(String[] args) {
+    System.out.println("this is java program");
+    System.out.println("this is output of java program");
+    System.out.println(new Date());
+  }
+}
